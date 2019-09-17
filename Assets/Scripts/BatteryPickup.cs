60f1fc8c45ab181d6bb68aad5fc4ca8e0304f1f7
João Traini <joao.traini@gmail.com>
2019-09-17 00:41:53 -0300

    6_ZR_213

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    public float restoreAngle = 90f;
+    public float addLightIntensity = 1f;
+    
+    
+    private void OnTriggerEnter(Collider collider) 
+    {
+        /* No OnTriggerEnter, para chamar outro script, você vai usar o collider (vai vir como "other"),
+           e vai linkar ele direto com o GetComponent.
+           Lembrando que esse GetComponentInChildren tá falando do objeto que o BatteryPickup tá procurando.
+           No caso, o FlashLightSystem é FILHO do PLAYER.
+           O BatteryPickup vai procurar NO PLAYER, e não nele mesmo.
+           Isso é importante para poder usar o GetComponent na maioria dos casos. */
+        collider.GetComponentInChildren<FlashLightSystem>().RestoreAngle(restoreAngle);
+        collider.GetComponentInChildren<FlashLightSystem>().AddLightIntensity(addLightIntensity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    public float lightDecay = .1f;
+    public float angleDecay = 1f;
+    public float minimumAngle = 40f;
+
+    public Light myLight;
+
+    private void Start() 
+    {
+        myLight = GetComponent<Light>();   
+    }
+
+    private void Update() 
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void AddLightIntensity(float addLightIntensity)
+    {
+        myLight.intensity += addLightIntensity;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngle) {
+            return;
+        } else {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
